fe3b26b24e3b922023b61a1590c16e5e29bd4c29
meloy <10772794+marcupan@users.noreply.github.com>
2025-11-22 13:00:25 +0200

    update configuration

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,11 +1,7 @@
-"use client";
-import {SignIn} from "@clerk/nextjs";
+import { SignIn } from '@clerk/nextjs';
 
-export default function SignInPage() {
-    return (
-        <div className="min-h-[60vh] flex items-center justify-center p-6">
-            <SignIn/>
-        </div>
-    );
+export const runtime = 'edge';
+
+export default function Page() {
+  return <SignIn />;
 }
-
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,11 +1,7 @@
-"use client";
-import {SignUp} from "@clerk/nextjs";
+import { SignUp } from '@clerk/nextjs';
 
-export default function SignUpPage() {
-    return (
-        <div className="min-h-[60vh] flex items-center justify-center p-6">
-            <SignUp/>
-        </div>
-    );
+export const runtime = 'edge';
+
+export default function Page() {
+  return <SignUp />;
 }
-
